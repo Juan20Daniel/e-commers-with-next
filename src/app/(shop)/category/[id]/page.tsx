9da5376d81864f66d0d7aca1eeef1a1c01bce1a5
Juan20Daniel <juandaniel312000@gmail.com>
2025-07-25 18:10:56 -0600

    Se agregó la ruta not found en categorias

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,18 @@
+import { notFound } from "next/navigation";
+interface Props {
+  params: Promise<{id:string}>
+}
 
-export default function CategoryPage() {
+const allowedParams = ['kids', 'men', 'women'];
+
+export default async function({params}:Props) {
+  const {id} = await params;
+  
+  if(!allowedParams.includes(id)) notFound();
+  
   return (
     <div>
-      <h1>Category Page</h1>
+      <h1>Category Page {id}</h1>
     </div>
   );
 }
